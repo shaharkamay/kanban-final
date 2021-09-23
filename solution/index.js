1daa9efa268b5da849fcb8daa16eaadc584b9652
shaharkamay <shahar.kamay@gmail.com>
2021-09-23 20:09:55 +0300

    added option to move tasks between lists with alt+1/2/3 keys and added an option to edit task by double clicking on it (passed both tests)

--- solution/index.js
+++ solution/index.js
@@ -26,40 +26,93 @@ function handleAddTask(e) {
         taskInput = e.target.previousElementSibling;
         ul = e.target.parentElement.previousElementSibling;
     }
-    const dataType = taskInput.closest("section").dataset.type;
+    const listType = taskInput.closest("section").dataset.listType;
 
     if(!taskInput.value) {
         alert("Invalid input!");
         return;
     }
     
-    let allTasksObj = {};
-    let listArr = [];
-    if(localStorage.getItem("tasks")) {
-        const tasksJson = localStorage.getItem("tasks");
-        allTasksObj = JSON.parse(tasksJson);
-        if(allTasksObj.hasOwnProperty(dataType)) {
-            listArr =  allTasksObj[dataType];
-        }
-    }
-
-    // const taskObj = {
-    //     id: generateId(listArr),
-    //     task: taskInput.value,
-    //     date: new Date().toLocaleString(),
-    // }
-
-    // listArr.push(taskObj);
-    listArr.push(taskInput.value);
-
-    allTasksObj[dataType] = listArr;
-
-
-    localStorage.setItem("tasks", JSON.stringify(allTasksObj));
-
-    const li = createElement("li", [taskInput.value], ["task"], {});
+    addTask(taskInput.value, listType);
+    
+    const li = createElement("li", [taskInput.value], ["task"], {"data-task": taskInput.value});
     ul.prepend(li);
     taskInput.value = "";
+}
+
+
+/*
+this function handle the task edit
+*/
+function handleEditTask(e) {
+    if(e.target.tagName !== "LI") return;
+
+    const liTask = e.target;
+    liTask.contentEditable = true;
+    liTask.focus();
+
+    const listType = liTask.closest("section").dataset.listType;
+
+    if(e.key === "Enter") {
+        updateTask(liTask.textContent, liTask.dataset.task, listType);
+        liTask.blur();
+    }
+}
+
+
+/*
+this function handles the task movements between lists
+*/
+function handleMoveTask(e) {
+    const mouseOverElements = document.querySelectorAll(":hover");
+    const liMouseOn = mouseOverElements[mouseOverElements.length - 1];
+    
+    if(liMouseOn.tagName !== "LI" || e.key !== 'Alt') return;
+
+    altKeyDownEventHandler(e);
+}
+function altKeyDownEventHandler(e) {
+    const mouseOverElements = document.querySelectorAll(":hover");
+    const liMouseOn = mouseOverElements[mouseOverElements.length - 1];
+    
+    document.addEventListener("keydown", numberKeyDownEventHandler);
+    document.addEventListener("keyup", altKeyUpEventHandler);
+}
+
+function altKeyUpEventHandler(e) {
+    const keyUp = e.key;
+
+    if(keyUp === "Alt") {
+        document.removeEventListener("keydown", numberKeyDownEventHandler);
+        document.removeEventListener("keyup", altKeyUpEventHandler);
+    }
+}
+
+function numberKeyDownEventHandler(e) {
+    const keyPressed = e.key
+
+    const mouseOverElements = document.querySelectorAll(":hover");
+    const liMouseOn = mouseOverElements[mouseOverElements.length - 1];
+
+    if(!liMouseOn || liMouseOn.tagName !== "LI") return;
+
+    const task = liMouseOn.textContent;
+    const previousListType = liMouseOn.closest("section").dataset.listType;
+    let newListType = null;
+
+    switch(keyPressed) {
+        case '1':
+            newListType = "todo";
+            break;
+        case '2':
+            newListType = "in-progress";
+            break;
+        case '3':
+            newListType = "done";
+            break;
+    }
+
+    if(newListType !== null) moveTask(task, previousListType, newListType);
 }
 
 
@@ -102,21 +155,31 @@ function generateId(arr) {
     return max + 1;
 }
 
+function setLocalStorage() {
+    if(!localStorage.getItem("tasks")) {
+        const setObjTasks = {
+            "todo": [],
+            "in-progress": [],
+            "done": [],
+        }
+        localStorage.setItem("tasks", JSON.stringify(setObjTasks));
+    }
+}
+
 /*
 this function display the tasks lists to DOM
 */
 function generateLists() {
     const ulLists = document.querySelectorAll("ul");
     for(const list of ulLists) {
-        const dataType = list.closest("section").dataset.type;
-        if(localStorage.getItem("tasks")) {
-            const allTasksObj = JSON.parse(localStorage.getItem("tasks"));
-            const tasksList = allTasksObj[dataType];
-            sortListByDate(tasksList);
-            for(const task of tasksList) {
-                const li = createElement("li", [task.task], ["task"], {});
-                list.append(li);
-            }
+        while(list.firstChild) list.removeChild(list.firstChild);
+        const listType = list.closest("section").dataset.listType;
+        const allTasksObj = JSON.parse(localStorage.getItem("tasks"));
+        const tasksList = allTasksObj[listType];
+        // sortListByDate(tasksList);
+        for(const task of tasksList) {
+            const li = createElement("li", [task], ["task"], {"data-task": task});
+            list.append(li);
         }
     }
 }
@@ -124,17 +187,19 @@ function generateLists() {
 /*
 this function sort a list by the task's date-time
 */
-function sortListByDate(list) {
-    return list.sort((a, b) => {
-        if(Date.parse(a.date) > Date.parse(b.date)) return -1;
-    });
-}
+// function sortListByDate(list) {
+//     return list.sort((a, b) => {
+//         if(Date.parse(a.date) > Date.parse(b.date)) return -1;
+//     });
+// }
 
 /*
 page structure
 */
 ( () => {
 
+    setLocalStorage();
+    
     document.addEventListener("DOMContentLoaded", () => {
         const savedTheme = localStorage.getItem("theme") || "auto";
     
@@ -149,12 +214,68 @@ page structure
             applyTheme(this.value);
           });
     });
-    
+
     const inputTasksElements = document.querySelectorAll(".add-task > input");
     for(const input of inputTasksElements) input.addEventListener("keypress", handleAddTask);
 
+    const ulLists = document.querySelectorAll("ul");
+    
     const listsSection = document.getElementById("lists");
     listsSection.addEventListener("click", handleAddTask);
+    listsSection.addEventListener("dblclick", handleEditTask)
+    document.addEventListener("keypress", handleEditTask)
+    document.addEventListener("keydown", handleMoveTask);
 
     generateLists();
+
+
 })();
+
+
+
+
+/*
+Local Storage functions
+*/
+function addTask(task, listType) {
+    const tasksJson = localStorage.getItem("tasks");
+    const allTasksObj = JSON.parse(tasksJson);
+    const listArr =  allTasksObj[listType];
+
+    listArr.unshift(task);
+    allTasksObj[listType] = listArr;
+
+    localStorage.setItem("tasks", JSON.stringify(allTasksObj));
+}
+
+function updateTask(newTask, oldTask, listType) {
+    const tasksJson = localStorage.getItem("tasks");
+    const allTasksObj = JSON.parse(tasksJson);
+    const listArr =  allTasksObj[listType];
+
+    const i = listArr.findIndex(x => x === oldTask);
+    listArr[i] = newTask;
+    allTasksObj[listType] = listArr;
+
+    localStorage.setItem("tasks", JSON.stringify(allTasksObj));
+}
+
+function removeTask(task, listType) {
+    const tasksJson = localStorage.getItem("tasks");
+    const allTasksObj = JSON.parse(tasksJson);
+    const listArr =  allTasksObj[listType];
+
+    const i = listArr.findIndex(x => x === task);
+    listArr.splice(i, 1);
+
+    allTasksObj[listType] = listArr;
+
+    localStorage.setItem("tasks", JSON.stringify(allTasksObj));
+}
+
+function moveTask(task, previousListType, newListType) {
+    removeTask(task, previousListType);
+    addTask(task, newListType);
+
+    generateLists();
+}
